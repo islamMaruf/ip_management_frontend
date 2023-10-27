577cb55accc4dd4bceac63461d8033ac82c9dcbb
maruf.islam <maruf.islam@miaki.co>
2023-10-28 02:07:43 +0600

    feat: new api added

--- src/services/api/AuthService.js
+++ src/services/api/AuthService.js
@@ -3,8 +3,24 @@ import _axiosInstance from "../../plugins/axios";
 export default {
     async login(payload) {
         try {
-            let {data} =  await _axiosInstance.post('/api/auth/login', payload);
+            let { data } = await _axiosInstance.post('/api/auth/login', payload);
             return data;
+        } catch (error) {
+            console.log('hitted')
+            throw error;
+        }
+    },
+    async refreshToken(payload) {
+        try {
+            return await _axiosInstance.post('/api/auth/refresh');
+        } catch (error) {
+            throw error;
+        }
+    },
+    async getLoggedInUser(){
+        try {
+            let {data} =  await _axiosInstance.post('/api/auth/user-profile');
+            return data
         } catch (error) {
             throw error;
         }

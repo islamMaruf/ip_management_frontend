1001603892c79bc0211ad4c9fdb626f23da2ad66
maruf.islam <maruf.islam@miaki.co>
2023-10-28 17:25:39 +0600

    fix:interceptors issue fixed

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,78 +1,62 @@
+
+import AuthService from '@/services/api/AuthService';
+import CookieService from '@/services/core/CookieService';
 import axios from 'axios';
 import router from '@/router';
-import CookieService from '@/services/core/CookieService';
-import AuthService from '@/services/api/AuthService';
 
-const axiosInstance = axios.create({
-    baseURL: process.env.VUE_APP_BASE_URL,
+const axios_instance = axios.create({
     timeout: 1000,
+    baseURL: process.env.VUE_APP_BASE_URL
 });
 
-axiosInstance.interceptors.request.use(
-    addAuthorizationHeader,
-    error => Promise.reject(error)
-);
-
-axiosInstance.interceptors.response.use(
-    handleResponse,
-    handleErrorResponse
-);
-
-function addAuthorizationHeader(config) {
-    const token = CookieService.getCookie('token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-}
-
-async function handleResponse(response) {
-    return response;
-}
-
-async function handleErrorResponse(error) {
-    if (error.response) {
-        const status = error.response.status;
-        if (status === 401) {
-            return handleAuthenticationError(error);
-        } else if (status === 403 || status === 429) {
-            return handleForbiddenAndTooManyRequestError(error);
+axios_instance.interceptors.request.use(
+    (config) => {
+        const token = CookieService.getCookie('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
         }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
+);
 
-    return Promise.reject(error);
-}
 
-async function handleAuthenticationError(error) {
-    error.config.retries = error.config.retries || 0;
-    const maxRetries = 5;
-
-    if (error.config.retries < maxRetries) {
-        error.config.retries++;
-        const originalRequest = error.config;
-
-        try {
-            const response = await AuthService.refreshToken();
-
-            if (response.status === 200 && response.data.success && response.data.data.access_token) {
-                const newAccessToken = response.data.data.access_token;
-                CookieService.setCookie('token', newAccessToken, 1);
-                originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-                return axiosInstance(originalRequest);
+axios_instance.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    async (error) => {
+        console.log(error.response, error.response.status, router.history.current.meta.auth)
+        if (error.response && error.response.status == 401 && router.history.current.meta.auth) {
+            error.config.retries = error.config.retries || 0;
+            const maxRetries = 5;
+            if (error.config.retries < maxRetries) {
+                error.config.retries++;
+                const original_request = error.config;
+                try {
+                    const response = await AuthService.refreshToken();
+                    if (response.status == 200 && response.data.status && response.data.data.access_token) {
+                        let new_access_token = response.data.data.access_token;
+                        CookieService.setCookie('token', new_access_token, 1);
+                        original_request.headers.Authorization = `Bearer ${new_access_token}`;
+                    }
+                    
+                } catch (refreshError) {
+                    // throw error;
+                }
+                return axios_instance(original_request);
             }
-        } catch (refreshError) {
-            throw refreshError;
         }
+        else if (error.response && error.response.status == 403) {
+            CookieService.deleteCookie('token');
+            if (router.history.current.name != "Login") {
+                router.push({ name: 'Login' }).catch(console.error);
+            }
+            ;
+        }
+        return Promise.reject(error);
     }
-
-    return Promise.reject(error);
-}
-
-function handleForbiddenAndTooManyRequestError(error) {
-    CookieService.deleteCookie('token');
-    if (router.name !== 'Login') {
-        router.push('/');
-    }
-}
-
-export default axiosInstance;
+);
+export default axios_instance;
